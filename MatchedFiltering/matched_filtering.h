3f2fd29bef40ca3d38034cff9fa5daf950cce05c
alansantoyo <axs4549@mavs.uta.edu>
2026-04-22 13:05:25 -0500

    updated CMakeLists.txt and introduced header file

---
+++ MatchedFiltering/matched_filtering.h
@@ -0,0 +1,24 @@
+#ifndef _MATCHED_FILTERING_H_
+#define _MATCHED_FILTERING_H_
+
+#include <iostream>
+#include <cuda_runtime.h>
+
+#define BLOCK_SIZE 512 //@@ You can change this
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort = true) {
+    if (code != cudaSuccess) {
+        std::cerr << "GPUassert: " << cudaGetErrorString(code) << " " << file << " " << line << std::endl;
+        if (abort) exit(code);
+    }
+}
+
+
+
+// Function declaration will go here once I figure it out
+
+
+
+
+#endif
