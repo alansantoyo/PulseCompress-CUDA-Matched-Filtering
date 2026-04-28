948d1b054c438b2c8ccd12ea998248e919f313ec
alansantoyo <axs4549@mavs.uta.edu>
2026-04-28 11:53:26 -0500

    switched from std::vector to regular arrays

--- MatchedFiltering/src/main.cpp
+++ MatchedFiltering/src/main.cpp
@@ -4,18 +4,25 @@
 int main(int argc, char **argv)
 {
 	
-	std::vector<float> data(50000000);
+	float *data = new float[50000000];
 	std::ifstream file("/home/axs4549/PulseCompress-CUDA-Matched-Filtering/MatchedFiltering/src/signal.bin", std::ios::binary);
 	if (!file.is_open())
 	{
-		std::cout << "ERROR: j \n\n";
+		std::cout << "ERROR: Signal file could not be opened.\n\n";
 		return -1;
 	}
-	file.read(reinterpret_cast<char*>(data.data()), 50000000 * sizeof(float));
+	file.read(reinterpret_cast<char*>(data), 50000000 * sizeof(float));
 	
-	for( int i = 0; i < 30; i++)
-		std::cout << data[i] << std::endl;
-
+	// Read the signal file -- done
+	// Read the filter file 
+	float *filter = new float[1024];
+	std::ifstream file1("/home/axs4549/PulseCompress-CUDA-Matched-Filtering/MatchedFiltering/src/filter.bin", std::ios::binary);
+	if (!file1.is_open())
+	{
+		std::cout << "ERROR: Filter file could not be opened.\n\n";
+		return -1;
+	}
+	file1.read(reinterpret_cast<char*>(filter), 1024 * sizeof(float));
 
 
 
