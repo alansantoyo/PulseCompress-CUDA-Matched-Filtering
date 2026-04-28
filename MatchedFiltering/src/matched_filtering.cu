f546aaeb370ac6460c8da4ec7c4dd18a6be2470c
alansantoyo <axs4549@mavs.uta.edu>
2026-04-27 21:56:06 -0500

    added skeleton for gpu launch configuration

--- MatchedFiltering/src/matched_filtering.cu
+++ MatchedFiltering/src/matched_filtering.cu
@@ -14,10 +14,18 @@ void matchedFilter_kernel(float *signal, float *filter, float *ouptut, int filte
 
 void matchedFilter(float *signal, float *filter, float *output, int filterSize, int signalSize)
 {
+    //@@ Allocate GPU memory here
 	
+    //@@ Copy memory to the GPU here
 
+    //@@ Initialize the grid and block dimensions here
 
+    //@@ Launch the GPU Kernel here
+    
 
+    //@@ Copy the GPU memory back to the CPU here
+
+    //@@ Free the GPU memory here	
 
 
 }
