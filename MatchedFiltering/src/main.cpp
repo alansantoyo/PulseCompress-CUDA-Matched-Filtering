95fdb833b2cf512ab8b3ace353ec48012c8d6f92
alansantoyo <axs4549@mavs.uta.edu>
2026-04-27 22:32:51 -0500

    reading in signal file

--- MatchedFiltering/src/main.cpp
+++ MatchedFiltering/src/main.cpp
@@ -2,7 +2,18 @@
 
 int main(int argc, char **argv)
 {
+	float val;
+	std::ifstream file("signal.bin", std::ios:binary);
 
+	file.seekg(0, std::ios::end);
+	std::streamsize size = file.tellg();
+	file.seekg(0, std::ios::beg);
+	
+	std::vector<float> buffer(size / sizeof(float));
+	file.read(reinterpret_cast<char*>(buffer.data()), size);
+
+	for( int i = 0; i < 5; i++)
+		std::cout << buffer[i] << endl;
 
 
 
