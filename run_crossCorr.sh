4a08825b833eda17cf411f6100f47f0cd3fd93df
alansantoyo <axs4549@mavs.uta.edu>
2026-04-28 11:22:19 -0500

    added debugger flags to ease debugging

--- run_crossCorr.sh
+++ run_crossCorr.sh
@@ -3,11 +3,11 @@
 #SBATCH --gres=gpu:1
 
 
-cmake -B build
+cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
 
 echo "********************************************"
 echo "** Computing cross correlation for signal **"
 echo "********************************************"
 
-
+./bin/matched_filter
