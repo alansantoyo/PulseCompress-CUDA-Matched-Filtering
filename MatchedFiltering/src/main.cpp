47004e2a0c6a1beca36078075220bbba9d2af3e5
alansantoyo <axs4549@mavs.uta.edu>
2026-04-28 11:22:49 -0500

    fixed file path for file opening

--- MatchedFiltering/src/main.cpp
+++ MatchedFiltering/src/main.cpp
@@ -1,19 +1,20 @@
 #include "matched_filtering.h"
+#include <fstream>
 
 int main(int argc, char **argv)
 {
-	float val;
-	std::ifstream file("signal.bin", std::ios:binary);
-
-	file.seekg(0, std::ios::end);
-	std::streamsize size = file.tellg();
-	file.seekg(0, std::ios::beg);
 	
-	std::vector<float> buffer(size / sizeof(float));
-	file.read(reinterpret_cast<char*>(buffer.data()), size);
-
-	for( int i = 0; i < 5; i++)
-		std::cout << buffer[i] << endl;
+	std::vector<float> data(50000000);
+	std::ifstream file("/home/axs4549/PulseCompress-CUDA-Matched-Filtering/MatchedFiltering/src/signal.bin", std::ios::binary);
+	if (!file.is_open())
+	{
+		std::cout << "ERROR: j \n\n";
+		return -1;
+	}
+	file.read(reinterpret_cast<char*>(data.data()), 50000000 * sizeof(float));
+	
+	for( int i = 0; i < 30; i++)
+		std::cout << data[i] << std::endl;
 
 
 
