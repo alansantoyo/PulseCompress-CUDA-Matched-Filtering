becece87efa679993c6557ee1d3c03cc65b58b25
alansantoyo <axs4549@mavs.uta.edu>
2026-04-28 12:03:08 -0500

    finished setup for kernel in main.cpp

--- MatchedFiltering/src/main.cpp
+++ MatchedFiltering/src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
 	
-	float *data = new float[50000000];
+	float *data = new float[SIG_SIZE];
 	std::ifstream file("/home/axs4549/PulseCompress-CUDA-Matched-Filtering/MatchedFiltering/src/signal.bin", std::ios::binary);
 	if (!file.is_open())
 	{
@@ -14,8 +14,8 @@ int main(int argc, char **argv)
 	file.read(reinterpret_cast<char*>(data), 50000000 * sizeof(float));
 	
 	// Read the signal file -- done
-	// Read the filter file 
-	float *filter = new float[1024];
+	// Read the filter file -- done 
+	float *filter = new float[FILTER_SIZE];
 	std::ifstream file1("/home/axs4549/PulseCompress-CUDA-Matched-Filtering/MatchedFiltering/src/filter.bin", std::ios::binary);
 	if (!file1.is_open())
 	{
@@ -23,6 +23,17 @@ int main(int argc, char **argv)
 		return -1;
 	}
 	file1.read(reinterpret_cast<char*>(filter), 1024 * sizeof(float));
+
+	float *result = new float[SIZE_SIZE + FILTER_SIZE - 1];
+
+	// allocated memory for result -- done
+	//
+	// Next, call the function.
+
+	matchedFilter(data, filter, result, FILTER_SIZE, SIG_SIZE);
+
+	// After I have the result, make a binary file, and display it using either matlab or python
+	
 
 
 
