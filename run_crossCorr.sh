33c5b1a19a06cafe7303c16e782da04c61e256e5
alansantoyo <axs4549@mavs.uta.edu>
2026-04-30 22:14:57 -0500

    fixed how the program gets executed in bash script

--- run_crossCorr.sh
+++ run_crossCorr.sh
@@ -6,8 +6,13 @@
 cmake -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
 
-echo "********************************************"
-echo "** Computing cross correlation for signal **"
-echo "********************************************"
 
-./bin/matched_filter
+if [ -e "./bin/matched_filter" ]; then
+	echo "Launching the program..."
+	./bin/matched_filter
+	echo "********************************************"
+	echo "** Computing cross correlation for signal **"
+	echo "********************************************"
+
+	
+fi
