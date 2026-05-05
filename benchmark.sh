161cf9bf24551fb922317c41338ecca9a9ec0acd
alansantoyo <axs4549@mavs.uta.edu>
2026-05-05 11:48:19 -0500

    updated benchmarking script

--- benchmark.sh
+++ benchmark.sh
@@ -7,3 +7,5 @@ export TMPDIR=$HOME/tmp/ncu-lock
 ncu --set full -o profile_01_baseline -f ./bin/matched_filter
 
 ncu --set full -o profile_02_tiled -f ./bin/matched_filter_tiled
+
+ncu --set full -o profile_03_tiled_coarsened -f ./bin/matched_filter_tiled_coarsened
