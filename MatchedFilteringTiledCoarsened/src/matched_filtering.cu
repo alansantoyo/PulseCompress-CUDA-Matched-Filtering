46e54d1bbeef2b6d986ee028dfe31e365091bcf9
alansantoyo <axs4549@mavs.uta.edu>
2026-05-05 13:14:58 -0500

    no more compilation errors with the coarsened version

--- MatchedFilteringTiledCoarsened/src/matched_filtering.cu
+++ MatchedFilteringTiledCoarsened/src/matched_filtering.cu
@@ -9,9 +9,9 @@ void matchedFilter_kernel(float *signal, float *filter, float *output, int filte
 	int idx = blockIdx.x * blockDim.x + threadIdx.x;
 	float sum = 0.0f;
 	float sum2 = 0.0f;
-	__shared__ float s_signal[1535];	
+	__shared__ float s_signal[1024 + 1023];	
 
-	int spot_in_signal = blockIdx.x * blockDim.x;
+	int spot_in_signal = (blockIdx.x * blockDim.x) * 2;
 	int tileSize = (blockDim.x * 2) + 1023;	
 	for(int i = threadIdx.x; i < tileSize; i = i + blockDim.x) 
 	{
@@ -27,7 +27,6 @@ void matchedFilter_kernel(float *signal, float *filter, float *output, int filte
 	}
 	__syncthreads();
 	
-	
 	if ((idx * 2) < (signalSize - filterSize + 1) )
 	{
 		for(int i = 0; i < filterSize; i++) // For coarsening, I want to calculate two values at once.
@@ -40,9 +39,7 @@ void matchedFilter_kernel(float *signal, float *filter, float *output, int filte
 	if( ((idx * 2) + 1) < (signalSize - filterSize + 1) )
 		output[(idx * 2) + 1] = sum2;
 	
-
 	__syncthreads();
-
 }
 
 void matchedFilter(float *signal, float *filter, float *output, int filterSize, int signalSize)
@@ -50,7 +47,7 @@ void matchedFilter(float *signal, float *filter, float *output, int filterSize, 
     	//@@ Allocate GPU memory here
 	float *deviceSignal, *deviceOutput; // Deleted deviceFilter because of the global filter
 
-	int numBlocks = (( (signalSize - filterSize + 1) + BLOCK_SIZE - 1) / BLOCK_SIZE) / 2;
+	int numBlocks = std::ceil( (( (signalSize - filterSize + 1) + BLOCK_SIZE - 1) / BLOCK_SIZE) / 2 );
 
 	gpuErrchk( cudaMalloc((void **) &deviceSignal, signalSize * sizeof(float)) );
 	gpuErrchk( cudaMalloc((void **) &deviceOutput, (signalSize - filterSize + 1) * sizeof(float)) );
